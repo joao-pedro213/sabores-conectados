ef5ffbde8092c1e0403734295fe865acc37b37dd
jpcva <joao.capobiango213@gmail.com>
2025-05-14 22:13:26 -0300

    commit inicial

---
+++ src/main/java/com/postech/saboresconectados/SaboresconectadosApplication.java
@@ -0,0 +1,13 @@
+package com.postech.saboresconectados;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SaboresconectadosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SaboresconectadosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/postech/saboresconectados/SaboresconectadosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.postech.saboresconectados;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SaboresconectadosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
