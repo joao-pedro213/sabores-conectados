6637e06048f41004eef6b5f40aee4ae2ebe5b551
jpcva <joao.capobiango213@gmail.com>
2025-05-20 22:02:41 -0300

    Adicionando representação da entidade na camada de modelo

---
+++ src/main/java/com/postech/saboresconectados/usuario/model/Usuario.java
@@ -0,0 +1,28 @@
+package com.postech.saboresconectados.usuario.model;
+
+import com.postech.saboresconectados.usuario.model.enumerator.TipoUsuario;
+import lombok.Data;
+
+import java.util.Date;
+import java.util.UUID;
+
+@Data
+public class Usuario {
+
+    private UUID id;
+
+    private String nome;
+
+    private TipoUsuario tipoUsuario;
+
+    private String email;
+
+    private String login;
+
+    private String senha;
+
+    private String endereco;
+
+    private Date ultimaAlteracao;
+
+}
