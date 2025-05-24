a5a040c12825de44ccce4d685a58bd69606858d9
João Pedro Capobiango <61262617+joao-pedro213@users.noreply.github.com>
2025-05-24 17:36:30 -0300

    [Usuário] Endpoint para atualizar Usuário (#6)
    * Adicionando método para atualizar Usuário na camada Repository
    
    * Adicionando método para atualizar Usuário na camada Service
    
    * Adicionando nova Exception para lidar com casos onde o recurso não é encontrado na camada de dados
    
    * Adicionando novo DTO e modificando Mapper
    
    * Adicionando método para atualizar Usuário na camada Controller
    
    * Incluindo tratamento de RuntimeException genérico na classe GlobalExceptionHandler

--- src/main/java/com/postech/saboresconectados/usuario/controller/UsuarioController.java
+++ src/main/java/com/postech/saboresconectados/usuario/controller/UsuarioController.java
@@ -1,7 +1,8 @@
 package com.postech.saboresconectados.usuario.controller;
 
-import com.postech.saboresconectados.usuario.dto.NewUsuarioDto;
-import com.postech.saboresconectados.usuario.dto.UsuarioDto;
+import com.postech.saboresconectados.usuario.dto.NewUsuarioRequestDto;
+import com.postech.saboresconectados.usuario.dto.UpdateUsuarioRequestDto;
+import com.postech.saboresconectados.usuario.dto.UsuarioResponseDto;
 import com.postech.saboresconectados.usuario.mapper.UsuarioMapper;
 import com.postech.saboresconectados.usuario.model.Usuario;
 import com.postech.saboresconectados.usuario.service.UsuarioService;
@@ -9,7 +10,14 @@ import jakarta.validation.Valid;
 import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.UUID;
 
 @RestController
 @RequestMapping("/usuario")
@@ -19,11 +27,20 @@ public class UsuarioController {
     private UsuarioService usuarioService;
 
     @PostMapping
-    public ResponseEntity<UsuarioDto> register(@RequestBody @Valid NewUsuarioDto newUsuarioDto) {
-        Usuario newUsuario = UsuarioMapper.newUsuarioDtoToUsuario(newUsuarioDto);
-        newUsuario = this.usuarioService.create(newUsuario);
-        UsuarioDto usuarioDto = UsuarioMapper.usuarioToUsuarioDto(newUsuario);
-        return ResponseEntity.status(HttpStatus.CREATED).body(usuarioDto);
+    public ResponseEntity<UsuarioResponseDto> create(@RequestBody @Valid NewUsuarioRequestDto newUsuarioRequestDto) {
+        Usuario usuario = UsuarioMapper.newUsuarioRequestDtoToUsuario(newUsuarioRequestDto);
+        return ResponseEntity
+                .status(HttpStatus.CREATED)
+                .body(UsuarioMapper.usuarioToUsuarioResponseDto(this.usuarioService.create(usuario)));
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Void> update(
+            @PathVariable String id,
+            @RequestBody @Valid UpdateUsuarioRequestDto updateUsuarioRequestDto) {
+        Usuario usuario = UsuarioMapper.updateUsuarioRequestDtoToUsuario(updateUsuarioRequestDto);
+        this.usuarioService.update(UUID.fromString(id), usuario);
+        return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
     }
 
 }
