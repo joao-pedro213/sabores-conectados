100c9f157e18570d42a6526d8b7afea6f1c07da3
João Pedro Capobiango <61262617+joao-pedro213@users.noreply.github.com>
2025-05-26 21:47:08 -0300

    [Usuário] Endpoint para buscar Usuário por id (#7)
    * Implementando método para buscar usuário por id na camada Repository
    
    * Implementando método para buscar usuário por id na camada Service
    
    * Implementando método para buscar usuário por id na camada Controller
    
    * Incluindo método na classe TipoUsuario para fazer o parse de uma string para um item do Enum

--- src/main/java/com/postech/saboresconectados/usuario/model/enumerator/TipoUsuario.java
+++ src/main/java/com/postech/saboresconectados/usuario/model/enumerator/TipoUsuario.java
@@ -2,6 +2,8 @@ package com.postech.saboresconectados.usuario.model.enumerator;
 
 import lombok.Getter;
 
+import java.util.Arrays;
+
 @Getter
 public enum TipoUsuario {
 
@@ -12,6 +14,14 @@ public enum TipoUsuario {
 
     TipoUsuario(String value) {
         this.value = value;
+    }
+
+    public static TipoUsuario fromValue(String value) {
+        return Arrays
+                .stream(TipoUsuario.values())
+                .filter((tipo) -> tipo.getValue().equals(value))
+                .findFirst()
+                .orElseThrow(() -> new IllegalArgumentException("O valor de TipoUsuario informado não é válido"));
     }
 
 }
